e5223e09a5c17d56678035a217de624275ce640d
amincoding <aminabdedaiem@gmail.com>
2024-09-23 21:06:46 +0100

    testing deployment

--- elanwar_agancy_flutter/lib/utils/api_client.dart
+++ elanwar_agancy_flutter/lib/utils/api_client.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:elanwar_agancy_client/elanwar_agancy_client.dart';
 import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
@@ -15,14 +14,35 @@ abstract class ApiClient {
 class ApiClientImplementation extends ApiClient {
   @override
   Future<void> init() async {
+    String localIp = await _getLocalIpAddress();
+
     client = Client(
       Platform.isAndroid
-          ? 'http://192.168.1.40:8080/'
-          : 'http://$localhost:8080/',
+          ? 'http://$localIp:8080/' // Using the local IP address
+          : 'http://localhost:8080/',
       authenticationKeyManager: FlutterAuthenticationKeyManager(),
     )..connectivityMonitor = FlutterConnectivityMonitor();
 
     sessionManager = SessionManager(caller: client.modules.auth);
     await sessionManager.initialize();
+  }
+
+  Future<String> _getLocalIpAddress() async {
+    try {
+      List<NetworkInterface> interfaces = await NetworkInterface.list(
+        includeLoopback: false,
+        type: InternetAddressType.IPv4,
+      );
+      for (var interface in interfaces) {
+        for (var addr in interface.addresses) {
+          if (!addr.isLoopback) {
+            return addr.address; // Return the first non-loopback IP address
+          }
+        }
+      }
+    } catch (e) {
+      print('Failed to get IP address: $e');
+    }
+    return '127.0.0.1'; // Fallback to localhost if IP address cannot be determined
   }
 }
